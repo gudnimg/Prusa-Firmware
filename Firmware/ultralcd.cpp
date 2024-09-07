329e8e897fe054ba8cd9a25e4de0c5daef4c1a01
gudnimg <gudni.m.g@gmail.com>
2024-09-07 11:42:21 +0000

    wizard: show message before rendering a new menu

--- Firmware/ultralcd.cpp
+++ Firmware/ultralcd.cpp
@@ -3920,8 +3920,8 @@ void lcd_wizard(WizState state)
 			}
 			break;
 		case S::Preheat:
+			lcd_show_fullscreen_message_and_wait_P(_T(MSG_SEL_PREHEAT_TEMP));
 			menu_goto(lcd_preheat_menu, 0, true);
-		    lcd_show_fullscreen_message_and_wait_P(_T(MSG_SEL_PREHEAT_TEMP));
 		    end = true; // Leave wizard temporarily for lcd_preheat_menu
 		    break;
 		case S::LoadFilHot:
